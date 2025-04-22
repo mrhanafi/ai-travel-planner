e923dec226e51e772408c17a1006299da2730d35
unknown <muhdhanafirahman@gmail.com>
2025-04-22 10:39:22 +0800

    add generate trip and gif

---
+++ app/create-trip/generate-trip.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const GenerateTrip = () => {
+  return (
+    <View>
+      <Text>GenerateTrip</Text>
+    </View>
+  );
+};
+
+export default GenerateTrip;
